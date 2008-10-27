e06fec57224d54f9d46d9e4664726132b5128a42
solrex <solrex@b8839100-cf4c-0410-a34d-a92c6c0b7f09>
2008-10-27 13:34:44 +0000

    Updated weatherman.py to weather.com.cn.

--- scripts/python/weatherman.py
+++ scripts/python/weatherman.py
@@ -3,22 +3,29 @@
 import os
 import urllib
 
-city_list = ['南京','北京']
-send_to_self_cities = ['南京','北京']
+city_list = {
+    '南京': '101190101',
+    '北京': '101010100',
+    '郑州': '101180101',
+}
+my_cities = ['南京','北京']
+weather_users = {
+    'yyyyyyyy': '郑州',
+}
 
 class weather:
-    urlbase = "http://php.weather.sina.com.cn/search.php?city="
+    urlbase = "http://www.weather.com.cn/html/weather/"
     def __init__(self):
         pass
 
     def clear_html(self):
-        for city in city_list:
+        for city in city_list.keys():
             cmd = "rm -f %s.htm" % (city)
             os.popen(cmd)
 
     def get_html(self):
-        for city in city_list:
-            url = self.urlbase + urllib.quote(city.decode('utf8').encode('gbk'))
+        for city in city_list.keys():
+            url = self.urlbase + city_list[city] + '.shtml'
             cmd = "wget -O %s.htm %s" % (city, url)
             os.popen(cmd)
         return 0
@@ -27,7 +34,7 @@ class weather:
         for city in city_list:
             filename = city + ".htm"
             # Select specific part.
-            cmd = "sed -i -e '/SideBar/,500d;1,/Weather3DBlk/d;' %s" % (filename)
+            cmd = "sed -i -e '/surf/,1000d;1,/dd_0/d;' %s" % (filename)
             os.popen(cmd)
             # Remove html tag
             cmd = "sed -i -e 's/<[^>]*>//g;/<!--/d' %s" % (filename)
@@ -35,15 +42,11 @@ class weather:
             # Remove empty line, including &nbsp, &deg; from html
             cmd = "sed -i -e 's/&nbsp;//g;s/&deg;C//g;s/^\s*//g;/^$/d' %s" % (filename)
             os.popen(cmd)
-            # Change file encoding so we can edit the Chinese content of the file
-            # Why don't sina just use utf8... damn!
-            cmd = "iconv -f gb2312 -t utf8 %s > tempfile; mv tempfile %s" % (filename,filename)
-            os.popen(cmd)
             # Remove unwanted Chinese characters: remove ℃ and change ～ to -, and remove the details of the weather
-            cmd = "sed -i -e 's/℃//g;s/～/-/g;s/：//g;s/≤/小于/g;/\t\t/d' %s" % (filename)
+            cmd = "sed -i -e 's/℃//g;s/高温//g;s/低温//g;s/：//g;/\t\t/d' %s" % (filename)
             os.popen(cmd)
             # Changing content of the file
-            cmd = "sed -i -e 's/星期/周/g;s/..年//g;s/..月//g;s/日-..日/日/g;/%s/d;s/指数//g' %s" % (city,filename)
+            cmd = "sed -i -e 's/星期/周/g;/%s/d;s/指数//g' %s" % (city,filename)
             os.popen(cmd)
 
     def generate_msg(self,city):
@@ -51,30 +54,35 @@ class weather:
         filename = city + ".htm"
         fp = open(filename)
         msg += city + '天气'
-        fp.readline(); fp.readline(); fp.readline();
         for i in [1, 2]:
-          line = fp.readline().replace('\r\n','')
-          msg += '\n' + line + ':'
-          line = fp.readline().replace(' ', ',').replace('\r\n','')
-          msg = msg + line + '度,'
-          line = fp.readline().replace(' ', '').replace('\r\n','')
-          msg = msg + line
+          msg += '\n' + fp.readline().replace('\r\n', ':')
+          msg += fp.readline().replace('\r\n', ',')
+          hi_temp = fp.readline().replace('\r\n', '')
+          lo_temp = fp.readline().replace('\r\n', '')
+          msg += lo_temp + '-' + hi_temp + '度,'
+          msg += fp.readline().replace('\r\n', '')
         fp.close()
         return msg
 
 if __name__=="__main__":
-    start=weather()
+    start = weather()
     start.clear_html()
     start.get_html()
     start.parse_html()
     message = ""
 
     # Send the weather report to myself
-    for city in send_to_self_cities:
-        message = start.generate_msg(city) + '\n'
+    for city in my_cities:
+        message = start.generate_msg(city)
         print message
-        cmd = 'sendsms -f PHONENUM -p PASSWORD "%s"' % (message)
+        cmd = 'sendsms -f 13xxxxxxxxx -p ******** "%s"' % (message)
         os.system(cmd)
-        cmd = 'sendsms -f PHONENUM -p PASSWORD -t FETIONNUM "%s"' % (message)
+        cmd = 'sendsms -f 13xxxxxxxxx -p ******** -t yyyyyyyyy "%s"' % (message)
+        os.system(cmd)
+
+    for user in weather_users.keys():
+        message = start.generate_msg(weather_users[user])
+        print message
+        cmd = 'sendsms -f 13xxxxxxxxx -p ******** -t %s "%s"' % (user, message)
         os.system(cmd)
     start.clear_html()
