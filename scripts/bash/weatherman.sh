0a6fbd187e482159440c026be3dc5fba7e5805f3
solrex <solrex@b8839100-cf4c-0410-a34d-a92c6c0b7f09>
2008-11-23 13:04:32 +0000

    Updated weatherman bash script due to HTML change of weather.com.cn.

--- scripts/bash/weatherman.sh
+++ scripts/bash/weatherman.sh
@@ -26,30 +26,30 @@ parse_html()
     if [ $? -ne 0 ]; then
       sed -i -e '1,/c_1_1/d;/c_1_2/,$d;' $city.txt
     else
-      sed -i -e '1,/dd_0/d;/surf/,$d;' $city.txt
+      sed -i -e '1,/c_1_1/d;/surf/,$d;' $city.txt
+      sed -i -e '/dl class="right"/,/dd_0/d;' $city.txt
     fi
     # Remove HTML tags.
     sed -i -e 's/<[^>]*>//g;/<!--/d' $city.txt
     # Remove empty lines.
     sed -i -e 's/&nbsp;//g;s/&deg;C//g;s/^\s*//g;/^$/d' $city.txt
-    sed -i -e '11,$d;' $city.txt
     # Cut verbose words.
     sed -i -e 's/℃//g;s/高温//g;s/低温//g;s/：//g;s/指数//g;' $city.txt
-    sed -i -e 's/星期/周/g;'  $city.txt
+    sed -i -e 's/星期/周/g;s@/@\n@g;s/ //g;'  $city.txt
     # Format file content to SMS.
+    LANG=zh_CN.UTF-8
     MES="${city}天气\n"
-    MES=$MES`sed -n -e '1p' $city.txt | tr -d '\r\n'`:
-    MES=$MES`sed -n -e '2p' $city.txt | tr -d '\r\n'`,
-    MES=$MES`sed -n -e '4p' $city.txt | tr -d '\r\n'`到
-    MES=$MES`sed -n -e '3p' $city.txt | tr -d '\r\n'`度,
-    MES=$MES`sed -n -e '5p' $city.txt | tr -d '\r\n'`'\n'
-    MES=$MES`sed -n -e '6p' $city.txt | tr -d '\r\n'`:
-    MES=$MES`sed -n -e '7p' $city.txt | tr -d '\r\n'`,
-    MES=$MES`sed -n -e '9p' $city.txt | tr -d '\r\n'`到
-    MES=$MES`sed -n -e '8p' $city.txt | tr -d '\r\n'`度,
-    MES=$MES`sed -n -e '10p' $city.txt| tr -d '\r\n'`
-    echo -ne $MES >> $city.txt
-    sed -i -e '1,10d' $city.txt
+    MES=$MES`date -d tomorrow +%d`日周`date -d tomorrow +%a`:
+    MES=$MES`sed -n -e '1p' $city.txt | tr -d '\r\n'`,
+    MES=$MES`sed -n -e '3p' $city.txt | tr -d '\r\n'`到
+    MES=$MES`sed -n -e '2p' $city.txt | tr -d '\r\n'`度,
+    MES=$MES`sed -n -e '4p' $city.txt | tr -d '\r\n'`'\n'
+    MES=$MES`sed -n -e '5p' $city.txt | tr -d '\r\n'`:
+    MES=$MES`sed -n -e '6p' $city.txt | tr -d '\r\n'`,
+    MES=$MES`sed -n -e '8p' $city.txt | tr -d '\r\n'`到
+    MES=$MES`sed -n -e '7p' $city.txt | tr -d '\r\n'`度,
+    MES=$MES`sed -n -e '9p' $city.txt| tr -d '\r\n'`
+    echo -ne $MES > $city.txt
   done
 }
 
