287dcee9ab9a750a5775f481a8aaea5f5487b9fe
solrex <solrex@b8839100-cf4c-0410-a34d-a92c6c0b7f09>
2009-05-14 09:40:35 +0000

    Update gae test fetch.

--- webdev/gae/casnet/tasks/fetch.py
+++ webdev/gae/casnet/tasks/fetch.py
@@ -6,37 +6,33 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db
 
-class Greeting(db.Model):
-  author = db.UserProperty()
+class StatusDB(db.Model):
+  url = db.LinkProperty()
+  status_code = db.IntegerProperty()
   content = db.TextProperty()
   date = db.DateTimeProperty(auto_now_add=True)
   
 
-class MainPage(webapp.RequestHandler):
+class Fetch(webapp.RequestHandler):
   def get(self):
-    greeting = Greeting()
+    statusdb = StatusDB()
     result = urlfetch.fetch('http://blog.solrex.cn')
-
-   # if result.status_code == 200:
-   #   greeting.content = db.Text(result.content, encoding="utf8")
-   # else:
-    greeting.content = str(result.status_code)
-    greeting.put()
+    statusdb.url = 'http://blog.solrex.cn'
+    statusdb.status_code = result.status_code
+   #statusdb.content = str(result.status_code)
+    statusdb.put()
 
     self.response.out.write('<html><body>')
 
-    greetings = db.GqlQuery("SELECT * FROM Greeting ORDER BY date DESC LIMIT 10")
+    statusdbs = db.GqlQuery("SELECT * FROM StatusDB ORDER BY date DESC LIMIT 10")
 
-    for greeting in greetings:
-      if greeting.author:
-        self.response.out.write('<b>%s</b> wrote:' % greeting.author.nickname())
-      else:
-        self.response.out.write('An anonymous person wrote:')
+    for i in statusdbs:
+      self.response.out.write('%s returns at time %s' % (i.url, i.date))
       self.response.out.write('<blockquote>%s</blockquote>' %
-                              greeting.content)
+                              i.status_code)
 
 application = webapp.WSGIApplication(
-                                     [('/tasks/fetch', MainPage)],
+                                     [('/tasks/fetch', Fetch)],
                                      debug=True)
 
 def main():
