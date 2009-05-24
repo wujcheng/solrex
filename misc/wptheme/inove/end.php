47fe221d8911a12f2d101fd41cb7d616d2383128
solrex <solrex@b8839100-cf4c-0410-a34d-a92c6c0b7f09>
2009-05-24 04:55:23 +0000

    First commit my changes to inove theme.

--- misc/wptheme/inove/end.php
+++ misc/wptheme/inove/end.php
@@ -14,10 +14,10 @@
 				if($firstpost_year != $lastpost_year) {
 					$copyright .= '-'. $lastpost_year;
 				}
-				$copyright .= ' ';
+				$copyright .= ' <a href="http://solrex.cn">Solrex Yang</a>. ';
 
 				echo $copyright;
-				bloginfo('name');
+                _e('This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/3.0/us/"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-nc-nd/3.0/us/80x15.png" />Creative Commons United States License</a>.', 'inove');
 			}
 		?>
 	</div>
@@ -34,5 +34,34 @@
 
 <?php wp_footer(); ?>
 
+<!-- Include the Google Friend Connect javascript library. -->
+<script type="text/javascript" src="http://www.google.com/friendconnect/script/friendconnect.js"></script>
+<!-- Define the div tag where the gadget will be inserted. -->
+<div id="div-9078055125469082983"></div>
+<!-- Render the gadget into a div. -->
+<script type="text/javascript">
+var skin = {};
+skin['BORDER_COLOR'] = '#cccccc';
+skin['ENDCAP_BG_COLOR'] = '#e0ecff';
+skin['ENDCAP_TEXT_COLOR'] = '#333333';
+skin['ENDCAP_LINK_COLOR'] = '#0000cc';
+skin['ALTERNATE_BG_COLOR'] = '#ffffff';
+skin['CONTENT_BG_COLOR'] = '#ffffff';
+skin['CONTENT_LINK_COLOR'] = '#0000cc';
+skin['CONTENT_TEXT_COLOR'] = '#333333';
+skin['CONTENT_SECONDARY_LINK_COLOR'] = '#7777cc';
+skin['CONTENT_SECONDARY_TEXT_COLOR'] = '#666666';
+skin['CONTENT_HEADLINE_COLOR'] = '#333333';
+skin['POSITION'] = 'top';
+skin['DEFAULT_COMMENT_TEXT'] = '- \u7ed9\u6211\u7559\u8a00(\u65e5\u5fd7\u8bc4\u8bba\u8bf7\u5230\u5355\u7bc7\u65e5\u5fd7\u9875)-';
+skin['HEADER_TEXT'] = '\u7559\u8a00\u677f+\u6211\u7684 MiniBlog';
+google.friendconnect.container.setParentUrl('/' /* location of rpc_relay.html and canvas.html */);
+google.friendconnect.container.renderSocialBar(
+ { id: 'div-9078055125469082983',
+   site: '02071694419876609776',
+   'view-params':{"scope":"SITE","features":"video,comment","showWall":"true"}
+ },
+  skin);
+</script>
 </body>
 </html>
