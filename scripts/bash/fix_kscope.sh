c0cdb0a234de44236f498cc9d531d6357644f669
solrex <solrex@gmail.com>
2009-07-08 09:28:35 +0000

    Fix error in fix_kscope script.

--- scripts/bash/fix_kscope.sh
+++ scripts/bash/fix_kscope.sh
@@ -2,27 +2,6 @@
 # This script helps you install Kscope on Ubuntu 9.04.
 # You can also use it to fix "Kscope doesn't run in KDE4" bug.
 
-# If Kscope is not installed, install it.
-which kscope &> /dev/null
-if [ $? -ne 0 ]; then
-  echo "Installing kscope..."
-  sudo apt-get install kscope
-  if [ $? -ne 0 ]; then
-    wget http://archive.ubuntu.com/ubuntu/pool/universe/k/kscope/kscope_1.6.0-1_${ARCH}.deb
-    sudo dpkg -i kscope_*.deb
-    if [ $? -ne 0 ]; then
-      echo "Oops, some error happens..."
-      exit
-    fi
-  fi
-fi
-
-kscope -v &> /dev/null
-if [ $? -eq 0 ]; then 
-  echo "Kscope works fine."
-  exit
-fi
-
 echo "Determining machine hardware name... "
 MACHINE=`uname -m`
 case "$MACHINE" in
@@ -36,6 +15,23 @@ case "$MACHINE" in
     ARCH="i386"
     ;;
 esac
+
+# If Kscope is not installed, install it.
+which kscope &> /dev/null
+if [ $? -ne 0 ]; then
+  echo "Installing kscope..."
+  sudo apt-get install kscope || \
+  wget http://archive.ubuntu.com/ubuntu/pool/universe/k/kscope/kscope_1.6.0-1_${ARCH}.deb && \
+  sudo dpkg -i kscope_*.deb || \
+  sudo apt-get -fy install || \
+  echo "Oops, some error happens..."
+fi
+
+kscope -v &> /dev/null
+if [ $? -eq 0 ]; then 
+  echo "Kscope works fine."
+  exit
+fi
 
 echo "Downloading KDE3 libraries needed by kscope..."
 wget http://ftp.debian.org/debian/pool/main/k/kdebase/kate_3.5.9.dfsg.1-6_${ARCH}.deb
