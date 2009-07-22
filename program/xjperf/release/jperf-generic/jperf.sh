24aa6cdd7d75a916099e040886c772ee1d71f9ed
solrex <solrex@gmail.com>
2009-07-22 05:15:03 +0000

    Add missing jperf 2.0.2 files.

--- program/xjperf/release/jperf-generic/jperf.sh
+++ program/xjperf/release/jperf-generic/jperf.sh
@@ -28,4 +28,4 @@ real_path()
 }
 JPERF_DIR=`dirname $(real_path $0)`
 cd $JPERF_DIR
-java -classpath jperf2.0.0.jar:lib/forms-1.1.0.jar:lib/jcommon-1.0.10.jar:lib/jfreechart-1.0.6.jar:lib/swingx-2008_02_03.jar net.nlanr.jperf.JPerf
+java -classpath jperf.jar:lib/forms-1.1.0.jar:lib/jcommon-1.0.10.jar:lib/jfreechart-1.0.6.jar:lib/swingx-2008_02_03.jar net.nlanr.jperf.JPerf
