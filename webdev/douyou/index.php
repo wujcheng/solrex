e30782f1355a8529c868aa21baf67c666bffb0f0
solrex <solrex@b8839100-cf4c-0410-a34d-a92c6c0b7f09>
2009-07-28 14:39:34 +0000

    Update Douyou user interface.

--- webdev/douyou/index.php
+++ webdev/douyou/index.php
@@ -21,7 +21,7 @@
 <p>呃——我觉得写主页比写主程序还费劲。简单来说，这个东西就跟 Feedburner 的订阅数统计图标类似，利用<a href="http://douban.com">豆瓣</a>提供的 API 抓取你的豆瓣好友数量，并做成一个小图片出来让你可以放在自己的博客上秀一秀。比如下面就是我的豆瓣好友统计图标：</p>
 <p><a href="http://www.douban.com/people/Solrex/" title="豆瓣好友统计"><img src="http://solrex.cn/douyou/dc/Solrex" style="border: 0pt none ;" alt="豆瓣" height="26" width="88"></a></p>
 <p>你还可以移步到<a href="http://blog.solrex.cn">我的博客</a>右侧栏，看看豆瓣好友统计图标和其它统计图标共存的状况。本统计图标一天更新一次，因此统计数并不完全实时，这是为了减轻服务器负载，请理解。</p>
-<p>这个小项目完全是出于兴趣写成的，因此很简陋且维持在可用的水平上，我也没有更优化它的想法。在我服务器能承受的情况下我会尽量维持它，但本人不对服务的有效性和可用性做出任何承诺。</p>
+<p>这个小项目完全是出于兴趣写成的，因此很简陋且维持在可用的水平上，我也没有更优化它的想法。在我服务器能承受的情况下我会尽量维持它，但本人不对此服务做任何保证。</p>
 <p>我觉得这个服务本身应该由豆瓣提供，如果你是豆瓣的工作人员，觉得这个站点有趣并想在豆瓣中加入此服务的话，欢迎你和我联系，我将无偿提供所有的代码，仅仅希望在对应产品中加上一个 Thanks to 到我的链接。</p>
 <h3>生成图片</h3>
 <script type="text/javascript">
@@ -31,17 +31,16 @@ function show() {
   var code = document.getElementById("user_dc_code");
   img.innerHTML = '<img src="http://solrex.cn/douyou/dc/' + username + '" style="border: 0pt none ;" alt="豆瓣" height="26" width="88">';
   code.innerHTML = '<span style="color: rgb(0, 0, 128); font-weight: bold;">&lt;a</span> <span style="color: rgb(255, 0, 0);">href=</span><span style="color: rgb(0, 0, 255);">"http://www.douban.com/people/' + username + '"</span> <span style="color: rgb(255, 0, 0);">title=</span><span style="color: rgb(0, 0, 255);">"豆瓣好友统计"</span><span style="color: rgb(0, 0, 128); font-weight: bold;">&gt;&lt;img</span> <span style="color: rgb(255, 0, 0);">src=</span><span style="color: rgb(0, 0, 255);">"http://solrex.cn/douyou/dc/' + username + '"</span> <span style="color: rgb(255, 0, 0);">style=</span><span style="color: rgb(0, 0, 255);">"border: 0pt none ;"</span> <span style="color: rgb(255, 0, 0);">alt=</span><span style="color: rgb(0, 0, 255);">"豆瓣"</span> <span style="color: rgb(255, 0, 0);">height=</span><span style="color: rgb(0, 0, 255);">"26"</span> <span style="color: rgb(255, 0, 0);">width=</span><span style="color: rgb(0, 0, 255);">"88"</span><span style="color: rgb(0, 0, 128); font-weight: bold;">&gt;&lt;/a&gt;</span>';
-  var userinput = document.getElementById("username");
-  userinput.value = "豆瓣用户名，非 email，英文或数字";
 }
 </script>
-<p>输入豆瓣帐号：<input name="username" id="username" tabindex="1" value="豆瓣用户名，非 email，英文或数字" size="40" type="text">
-<input value="提交" tabindex="2" type="button" onclick="show()"></p>
-<p id="user_dc_img"></p>
+<p>输入豆瓣 UID：<input name="username" id="username" value="solrex" size="20" type="text">
+<input value="提交" type="button" onclick="show()">(豆瓣用户 UID，英文或数字，非登录 email 地址)</p>
+<p>（若没有即刻显示请稍等后多提交一次，服务器抓取信息可能有延迟。不知道自己的 UID 的话，可以登录豆瓣，查看自己的设置->username项。）</p>
+<p id="user_dc_img"><img src="http://solrex.cn/douyou/dc/solrex" style="border: 0pt none ;" alt="豆瓣" width="88" height="26"></p>
 <p>您可以把下面这段代码嵌入到您的博客或者主页中来显示豆瓣好友统计：</p>
-<p><blockquote id="user_dc_code">请先提交您的豆瓣帐号</blockquote></p>
+<p><blockquote id="user_dc_code"><span style="color: rgb(0, 0, 128); font-weight: bold;">&lt;a</span> <span style="color: rgb(255, 0, 0);">href=</span><span style="color: rgb(0, 0, 255);">"http://www.douban.com/people/solrex"</span> <span style="color: rgb(255, 0, 0);">title=</span><span style="color: rgb(0, 0, 255);">"豆瓣好友统计"</span><span style="color: rgb(0, 0, 128); font-weight: bold;">&gt;&lt;img</span> <span style="color: rgb(255, 0, 0);">src=</span><span style="color: rgb(0, 0, 255);">"http://solrex.cn/douyou/dc/solrex"</span> <span style="color: rgb(255, 0, 0);">style=</span><span style="color: rgb(0, 0, 255);">"border: 0pt none ;"</span> <span style="color: rgb(255, 0, 0);">alt=</span><span style="color: rgb(0, 0, 255);">"豆瓣"</span> <span style="color: rgb(255, 0, 0);">height=</span><span style="color: rgb(0, 0, 255);">"26"</span> <span style="color: rgb(255, 0, 0);">width=</span><span style="color: rgb(0, 0, 255);">"88"</span><span style="color: rgb(0, 0, 128); font-weight: bold;">&gt;&lt;/a&gt;</span></blockquote></p>
 <h3>联系我</h3>
-<p>请到<a href="http://solrex.cn">我的主页</a>寻找我的联系方式。</p>
+<p>请到<a href="http://solrex.cn">我的主页</a>寻找我的联系方式。如果您有什么问题或建议，也可以到<a href="http://blog.solrex.cn/articles/douyou-count.html">这篇博客</a>下面评论指出。</p>
 <h3>我的其它链接</h3>
 <ul>
 <li><a href="http://blog.solrex.cn">Solrex Shuffling</a>: 我的博客</li>
