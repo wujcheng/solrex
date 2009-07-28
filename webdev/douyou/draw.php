efe7212e1e9ee2b02a7a705c103ead076eacf555
solrex <solrex@gmail.com>
2009-07-28 06:24:18 +0000

    Add douyou project.

---
+++ webdev/douyou/draw.php
@@ -0,0 +1,9 @@
+<?php
+$string = "798";
+$im     = imagecreatefrompng("templates/douyou.png");
+$fcolor = imagecolorallocate($im, 0x42, 0x42, 0x42);
+$px     = 43 - 6 * strlen($string);
+imagestring($im, 2, $px, 2, $string, $fcolor);
+imagepng($im, "test.png");
+imagedestroy($im);
+?>
