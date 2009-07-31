88bbe1c715a139e2bac89d6b386d4795f6540ac3
solrex <solrex@gmail.com>
2009-07-31 01:41:59 +0000

    Initial import of pythius.

---
+++ program/pythius/pythius/__init__.py
@@ -0,0 +1,22 @@
+"""
+    Pythius - Package Initialization
+
+    Copyright (c) 2001 by Jürgen Hermann <jh@web.de>
+    All rights reserved, see LICENSE for details.
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    $Id: __init__.py,v 1.1 2001/11/05 22:34:03 jhermann Exp $
+"""
+__version__ = "$Revision: 1.1 $"[11:-2]
+
+# Imports
+
