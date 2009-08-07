6cc4e889c516ea22d7b3e1270516300bfe5c9e85
solrex <solrex@b8839100-cf4c-0410-a34d-a92c6c0b7f09>
2009-08-07 17:49:02 +0000

    Tohr has been moved to sourceforge as a single project. Remove tohr and gaeproxy from here.

--- program/tohr/proxy/python/update-ca.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-CADIR=./ca
-openssl genrsa -out $CADIR/ca.key 1024
-openssl req -batch -new -key $CADIR/ca.key -out $CADIR/ca.csr -subj "/C=CN/ST=BJ/L=BJ/O=The Onion HTTP Router/OU=Tohr Certificate Authority/CN=Tohr CA"
-openssl x509 -req -days 1095 -text -in $CADIR/ca.csr -signkey $CADIR/ca.key -out $CADIR/ca.crt
-rm -f $CADIR/ca.csr
-firefox $CADIR/ca.crt
