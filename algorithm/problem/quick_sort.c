a6e8b4825749611a6d76836de40ff06b5fc0a799
solrex <solrex@b8839100-cf4c-0410-a34d-a92c6c0b7f09>
2009-09-16 10:53:52 +0000

    Add quick sort.

---
+++ algorithm/problem/quick_sort.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <assert.h>
+
+int partition(int a[], int left, int right, int pivot)
+{
+  int i, t, pIndex;
+  assert(a!=NULL);
+  t = a[pivot]; a[pivot] = a[right]; a[right] = t;
+  pIndex = left;
+  for (i=left; i<right; i++) {
+    if (a[i]<=a[right]) {
+      if (i!=pIndex) {
+        t = a[i]; a[i] = a[pIndex]; a[pIndex] = t;
+      }
+      pIndex++;
+    }
+  }
+  t = a[pIndex]; a[pIndex] = a[right]; a[right] = t;
+  return pIndex;
+}
+
+void qsort(int a[], int left, int right)
+{
+  int pivot;
+  assert(a!=NULL);
+  if (right > left) {
+    pivot = partition(a, left, right, left);
+    qsort(a, left, pivot-1);
+    qsort(a, pivot+1, right);
+  }
+}
+
+int main()
+{
+  int a[10], i;
+  for (i=0; i<10; i++) {
+    a[i] = 71*(i+1)%100;
+    printf("%d ", a[i]);
+  }
+  printf("\n");
+  qsort(a, 0, 9);
+  for (i=0; i<10; i++) {
+    printf("%d ", a[i]);
+  }
+  printf("\n");
+}
