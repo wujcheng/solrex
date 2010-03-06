5d14a35d42c981241248a9e117b12b5cc61603f4
Solrex <Solrex@b8839100-cf4c-0410-a34d-a92c6c0b7f09>
2010-03-06 15:13:41 +0000

    Update htpdate.

--- program/htpdate/bin/htpdate.py
+++ program/htpdate/bin/htpdate.py
@@ -11,11 +11,19 @@ def main():
   t = time.localtime(time.mktime(time.strptime(res_time,
                                  '%a, %d %b %Y %H:%M:%S %Z')) - time.timezone)
   time_str = time.strftime('%H:%M:%S', t)
+  local_time = time.asctime()
   t_exe = time.clock()
   centi_sec = (t_exe - t_rtt/2)*100
-  system('time ' + time_str + '.%2.0f' % centi_sec)
+  if centi_sec > 99:
+    centi_sec = 99
+  system('time %s.%2.0f' % (time_str, centi_sec))
   date_str = time.strftime('%Y-%m-%d', t)
-  system('date ' + date_str)
+  system('date %s' % date_str)
+  print 'LOCAL  TIME: ' + local_time
+  print 'SERVER TIME: ' + time.asctime(t)
+  print 'LOCAL  TIME: ' + time.asctime()
+  if (t_exe - t_rtt/2) >= 1:
+    print 'Round trip time is too long. Time error might be larger than 1 sec.'
 
 if __name__ == '__main__':
-    main()
+  main()
