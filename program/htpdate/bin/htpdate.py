c0b1073c6f2a66572cfff57e3f48ed02587cf8df
solrex <solrex@b8839100-cf4c-0410-a34d-a92c6c0b7f09>
2010-03-06 14:06:49 +0000

    Update htpdate.

--- program/htpdate/bin/htpdate.py
+++ program/htpdate/bin/htpdate.py
@@ -1,23 +1,21 @@
 #!/usr/bin/env python
-
 import httplib, time
 from os import system
 
 def main():
-  compensation = 0.5
-  conn = httplib.HTTPConnection("google.com")
+  conn = httplib.HTTPConnection('google.com')
   time.clock()
-  conn.request("HEAD", "")
-  t_rt = time.clock()
-  res = conn.getresponse()
-  server_datetime = res.getheader("date")
-  t = time.localtime(time.mktime(time.strptime(server_datetime, "%a, %d %b %Y %H:%M:%S %Z")) - time.timezone)
-  time_str = time.strftime("%H:%M:%S", t)
-  t_handle = time.clock()
-  t_handle = (t_handle - t_rt/2)*100
-  system('time ' + time_str + '.%2.0f' % t_handle)
-  date_str = time.strftime("%Y-%m-%d", t)
+  conn.request('HEAD', '')
+  t_rtt = time.clock()
+  res_time = conn.getresponse().getheader('date')
+  t = time.localtime(time.mktime(time.strptime(res_time,
+                                 '%a, %d %b %Y %H:%M:%S %Z')) - time.timezone)
+  time_str = time.strftime('%H:%M:%S', t)
+  t_exe = time.clock()
+  centi_sec = (t_exe - t_rtt/2)*100
+  system('time ' + time_str + '.%2.0f' % centi_sec)
+  date_str = time.strftime('%Y-%m-%d', t)
   system('date ' + date_str)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
