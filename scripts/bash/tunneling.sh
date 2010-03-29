f4329b8267b35bf19d5c50a9757fb42dd4f1b09c
solrex <solrex@b8839100-cf4c-0410-a34d-a92c6c0b7f09>
2010-03-29 02:14:20 +0000

    Update tunneling script.

--- scripts/bash/tunneling.sh
+++ scripts/bash/tunneling.sh
@@ -5,7 +5,8 @@ SERVER="0.0.0.0"
 USRNAME=""
 PORT="22"
 
-INFOURL="http://someserver.com/somepage"
+INFOURL="http://example.com"
+INFOURLBAK="http://example.org"
 
 SSHOPTS="-C -f -N -g -o PreferredAuthentications=publickey -o StrictHostKeyChecking=no"
 
@@ -15,8 +16,12 @@ get_server_info()
   info=`wget --timeout=2 --tries=3 -nv -O - $INFOURL 2> /dev/null |
         iconv -f gbk -t utf8 | sed -n "/{*}/s/.*{\(.*\)}.*/\1/p"`
   if [ -z $info ]; then
-    echo "BBS server error."
-    exit 1
+    info=`wget --timeout=5 --tries=3 -nv -O - $INFOURLBAK 2> /dev/null |
+        iconv -f gbk -t utf8 | sed -n "/{*}/s/.*{\(.*\)}.*/\1/p"`
+    if [ -z $info ]; then
+      echo "INFO server error."
+      exit 1
+    fi
   fi
   COMMAND=${info%%:*}
   SERVER=${info#*:}
@@ -34,13 +39,18 @@ tunneling_status()
 
 start_tunneling()
 {
+  ps -e | grep -q antinat
+  if [ $? -ne 0 ]; then
+    /usr/local/bin/antinat &
+  fi
+  ssh $SSHOPTS -R 1080:127.0.0.1:1080 ${USRNAME}@${SERVER} -p $PORT
   ssh $SSHOPTS -R 3128:127.0.0.1:3128 ${USRNAME}@${SERVER} -p $PORT
-  ssh $SSHOPTS -R 8022:127.0.0.1:22 ${USRNAME}@${SERVER} -p $PORT
+  ssh $SSHOPTS -R 22:127.0.0.1:22 ${USRNAME}@${SERVER} -p $PORT
 }
 
 failsafe_tunneling()
 {
-  ssh $SSHOPTS -R 18022:127.0.0.1:22 ${USRNAME}@${SERVER} -p $PORT
+  ssh $SSHOPTS -R 1022:127.0.0.1:22 ${USRNAME}@${SERVER} -p $PORT
 }
 
 stop_tunneling()
@@ -51,6 +61,7 @@ stop_tunneling()
 
 echo -n "[`date +%F\ %R`] "
 get_server_info
+echo -n "$SERVER "
 
 if [ $# -ne 0 ]; then
   COMMAND=$1
@@ -93,6 +104,17 @@ case "$COMMAND" in
     ;;
   starth | stoph | restarth | sleeph | failsafeh)
     echo "Not my business, sleeping."
+    ;;
+  gfw)
+    ssh $SSHOPTS -R 9090:127.0.0.1:9090 ${USRNAME}@${SERVER} -p $PORT
+    fuckgfw
+    ;;
+  vnc)
+    ssh $SSHOPTS -R 5951:127.0.0.1:5951 ${USRNAME}@${SERVER} -p $PORT
+    vncserver :51
+    ;;
+  svn)
+    ssh $SSHOPTS -R 3690:127.0.0.1:3690 ${USRNAME}@${SERVER} -p $PORT
     ;;
   *)
     echo "Unrecogenized server command ($COMMAND)."
